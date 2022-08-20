41544be554b44043e285f30fccd48829bb613748
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-08-20 16:16:48 +0300

    string concatenation task

---
+++ lists_task2.py
@@ -0,0 +1,6 @@
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s","lly"]
+
+list3 = [list1[0] + list2[0], list1[1] + list2[1], list1[2] + list2[2], list1[3] + list2[3]]
+print(list3)
+print(" ".join(list3)) # this is how you do it without the strings
